a3b7d85d4d903c78e56d052f4d247527c757d20e
Sixtus Onyedibe <sixtusonyedibe@gmail.com>
2024-08-16 04:46:39 +0100

    Restructured files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,17 @@
-import LeftNav from '../../components/dashboard/left-nav';
-
 export default function page() {
   return (
-    <div className='bg-[#F1F1F1] min-h-[100vh] w-full grid grid-cols-5 '>
-      <section className='col-span-1 pt-8'>
-        <LeftNav />
-      </section>
-      <section className='col-span-4 bg-white w-full h-full rounded-tl-3xl pt-8'>
-        <h1>Dashboard</h1>
-      </section>
+    <div>
+      {/* Welcome olivia */}
+      <div>
+        <div>
+          <h1>Welcome back, Olivia</h1>
+          <p>Track, manage and forcast your customers and orders.</p>
+        </div>
+        <div>
+          <button>Import</button>
+          <button>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
